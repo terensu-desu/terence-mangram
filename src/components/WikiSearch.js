bdaa48461803709aff1b12d306cd5e9701b367ea
Terence Mangram <tmangram@gmail.com>
2017-12-27 11:37:31 +0900

    Add feature to check if search has happened, will display hint if not

--- src/components/WikiSearch.js
+++ src/components/WikiSearch.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class WikiSearch extends Component {
 	constructor() {
 		super();
+		this.handleHintSpace = this.handleHintSpace.bind(this);
 		this.handleSearchResults = this.handleSearchResults.bind(this);
 	}
 
@@ -15,13 +16,30 @@ export default class WikiSearch extends Component {
 						<h5 className="no-margin-bot"><span>{item.title}</span></h5>
 					</div>
 					<div className="col s8">
-						<p className="truncate no-margin-bot">{item.extract}</p><a className="right" href={"https://en.wikipedia.org/?curid=" + item.pageid}>See more</a>
+						<p className="truncate no-margin-bot">{item.extract}</p>
+						<a className="right accent" href={"https://en.wikipedia.org/?curid=" + item.pageid}>See more</a>
 					</div>
 					<hr/>
 				</div>
 			)
 		});
 		return resultsMap;
+	}
+
+	handleHintSpace() {
+		return (
+			<div className="row center">
+				<div className="col s12">
+					<br/>
+					<br/>
+					<br/>
+					<h5>Search to see the top three results from Wikipedia.</h5>
+					<br/>
+					<br/>
+					<br/>
+				</div>
+			</div>
+		)
 	}
 
 	render() {
@@ -48,7 +66,7 @@ export default class WikiSearch extends Component {
 							</div>
 						</form>
 					</div>
-					{this.handleSearchResults()}
+					{this.props.searched ? this.handleSearchResults() : this.handleHintSpace()}
 				</div>
 			</div>
 		)
