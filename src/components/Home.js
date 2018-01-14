2e171188d02879166c2bbe8e08780e633255a690
Terence Mangram <tmangram@gmail.com>
2018-01-14 11:47:21 +0900

    Some styling updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,47 +6,51 @@ export default class Home extends Component {
 		return (
 			<main>
 				<div className="container-fluid banner-image">
-					<div className="row center">
+					<div className="row center no-margin-bot">
 						<img src="https://i.imgur.com/NPqHt2yb.jpg" alt="author" className="author-showcase circle responsive-img z-depth-2" />
 						<h1 className="banner-text no-margin-top">{data.banner.main}</h1>
 						<h3 className="banner-text">{data.banner.sub}</h3>
 					</div>
 				</div>
-				<div className="container">
-					<div className="card-panel">
-						<div className="row">
-							<div className="col s12 l2 center">
-								<h3><span>Bio</span></h3>
-								<a href="https://www.linkedin.com/in/terence-mangram-98141a34/">
-	                <span className="fa-stack fa-2x">
-	                  <i className="fa fa-square-o fa-stack-2x"></i>
-	                  <i className="fa fa-linkedin fa-stack-1x"></i>
-	                </span>
-	              </a>
-	              <a href="https://github.com/terensu-desu">
-	                <span className="fa-stack fa-2x">
-	                  <i className="fa fa-square-o fa-stack-2x"></i>
-	                  <i className="fa fa-github fa-stack-1x"></i>
-	                </span>
-	              </a>
-	              <a href="https://codepen.io/terensu-desu/">
-	                <span className="fa-stack fa-2x">
-	                  <i className="fa fa-square-o fa-stack-2x"></i>
-	                  <i className="fa fa-codepen fa-stack-1x"></i>
-	                </span>
-	              </a>
-	              <a href="https://www.flickr.com/photos/mynomadmind/">
-	                <span className="fa-stack fa-2x">
-	                  <i className="fa fa-square-o fa-stack-2x"></i>
-	                  <i className="fa fa-flickr fa-stack-1x"></i>
-	                </span>
-	              </a>
+				<div className="container about">
+						<div className="row" style={{"margin-top": "20px"}}>
+							<div className="col s12 m4 center bio">
+								<div className="card-panel">
+									<h3 className="no-margin-top"><span>Bio</span></h3>
+									<a href="https://www.linkedin.com/in/terence-mangram-98141a34/">
+		                <span className="fa-stack fa-2x">
+		                  <i className="fa fa-square-o fa-stack-2x"></i>
+		                  <i className="fa fa-linkedin fa-stack-1x"></i>
+		                </span>
+		              </a>
+		              <a href="https://github.com/terensu-desu">
+		                <span className="fa-stack fa-2x">
+		                  <i className="fa fa-square-o fa-stack-2x"></i>
+		                  <i className="fa fa-github fa-stack-1x"></i>
+		                </span>
+		              </a>
+		              <a href="https://codepen.io/terensu-desu/">
+		                <span className="fa-stack fa-2x">
+		                  <i className="fa fa-square-o fa-stack-2x"></i>
+		                  <i className="fa fa-codepen fa-stack-1x"></i>
+		                </span>
+		              </a>
+		              <a href="https://www.flickr.com/photos/mynomadmind/">
+		                <span className="fa-stack fa-2x">
+		                  <i className="fa fa-square-o fa-stack-2x"></i>
+		                  <i className="fa fa-flickr fa-stack-1x"></i>
+		                </span>
+		              </a>
+								</div>
 							</div>
-							<div className="col s12 l10">
-								<p className="article">{data.about}</p>
+							<div className="col s12 m8">
+								<div className="card-panel">
+									<p className="article">{data.about}</p>
+								</div>
 							</div>
+
+							
 						</div>
-					</div>
 					<div className="footer-copyright card-panel">
 						<div className="row no-margin-top no-margin-bot">
 							<div className="col s12">
