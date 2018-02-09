aa97e0d5ac1972a82fec4b8609ddf5b9b183b345
Terence Mangram <tmangram@gmail.com>
2018-02-09 23:05:38 +0900

    New structure, remove router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import Home from '../components/Home';
-import Projects from '../components/Projects';
-import MyApps from '../components/MyApps';
-import store from '../Store';
+import React, { Component } from "react";
+
+import Header from "../components/Header";
+//import Sidebar from "../components/Sidebar";
+import About from "../components/About";
+import MyApps from "../components/Apps/MyApps";
+import Projects from "../components/Projects";
+import Footer from "../components/Footer";
+import store from "../Store";
 
 export default class App extends Component {
 	constructor() {
@@ -22,21 +24,31 @@ export default class App extends Component {
 	// will return display data based on the current state language
 	// the returned data will become a prop for the component which called it
 	handleDisplayData() {
-		const language =  this.state.language;
+		const language = this.state.language;
 		return store.returnDisplayData(language);
 	}
 
 	// navigation and two routes, each passed display data based on current state language
-  render() {
-    return (
-    	<Router basename={process.env.PUBLIC_URL}>
-	      <div>   
-		      <Sidebar changeLanguage={this.changeLanguage} />
-	        <Route exact path="/" render={() => <Home displayData={this.handleDisplayData()} />} />
-	        <Route path="/projects" render={() => <Projects displayData={this.handleDisplayData()} />} />
-	        <Route path="/apps" render={() => <MyApps displayData={this.handleDisplayData()} />} />
-	      </div>
-	    </Router>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<div className="container">
+					{/*<Sidebar changeLanguage={this.changeLanguage} />*/}
+					<About displayData={this.handleDisplayData()} />
+					<MyApps displayData={this.handleDisplayData()} />
+					<Projects displayData={this.handleDisplayData()} />
+					<Footer />
+				</div>
+			</div>
+		);
+	}
 }
+
+
+/*
+
+<Router basename={process.env.PUBLIC_URL}>
+</Router>
+
+*/
