1de837dfe342b6f5cdda6a1895adecb255cf613b
Terence Mangram <tmangram@gmail.com>
2018-02-09 23:07:48 +0900

    Stateless component for footer info

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div className=" card-panel center">
+			<div className="row no-margin-top no-margin-bot">
+				<div className="col s12">
+					<p className="no-margin-top no-margin-bot center">Made by <a href="https://github.com/terensu-desu"><span>Terence Mangram</span></a>, built with <a href="https://reactjs.org/"><span>React.js</span></a> and <a  href="http://next.materializecss.com/"><span>MaterializeCSS</span>.</a></p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Footer;
