a9295f8255c0dc62d335f726d4802dcf1b9cdbca
Terence Mangram <tmangram@gmail.com>
2018-02-11 13:31:06 +0900

    Structure change, some styling changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 const Header = () => {
 	return (
 		<div className="container-fluid">
 			<div className="row center no-margin-bot banner-image">
-				<img src="https://i.imgur.com/NPqHt2yb.jpg" alt="author" className="author-showcase circle responsive-img z-depth-2" />
-				<h1 className="banner-text no-margin-top">Terence Mangram</h1>
-				<h3 className="banner-text">Bilingual Front-End Developer</h3>
+				<div className="col s2 offset-s3">
+					<img src="https://i.imgur.com/NPqHt2yb.jpg" alt="author" className="author-showcase circle responsive-img z-depth-2" />
+				</div>
+				<div className="col s4">
+					<h1 className="banner-text">Terence Mangram</h1>
+					<h3 className="banner-text">Bilingual Front-End Developer</h3>
+				</div>
 			</div>
 		</div>
 	)
