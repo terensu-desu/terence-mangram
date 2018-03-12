a820b9962cc47656be16593828a290c13cfdef12
Terence Mangram <tmangram@gmail.com>
2018-03-12 18:14:07 +0900

    Import redux requirements and store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { Provider } from "react-redux";
+
+import store from "./store/store";
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
