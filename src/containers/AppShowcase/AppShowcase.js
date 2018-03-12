a90af45090b3c286556533f328f344a9af892243
Terence Mangram <tmangram@gmail.com>
2018-03-13 00:27:47 +0900

    Move to containers due to being a stateful component

--- src/containers/AppShowcase/AppShowcase.js
+++ src/containers/AppShowcase/AppShowcase.js
@@ -1,28 +1,23 @@
 import React, { Component } from "react";
 import { CSSTransitionGroup } from "react-transition-group";
 
-import WeatherApp from "../../containers/WeatherApp/WeatherApp";
-import WikiApp from "../../containers/WikiApp/WikiApp";
-import LeaderboardApp from "../../containers/LeaderboardApp/LeaderboardApp";
-import RecipeApp from "../../containers/RecipeApp/RecipeApp";
+import WeatherApp from "../WeatherApp/WeatherApp";
+import WikiApp from "../WikiApp/WikiApp";
+import LeaderboardApp from "../LeaderboardApp/LeaderboardApp";
+import RecipeApp from "../RecipeApp/RecipeApp";
+import TwitchApp from "../TwitchApp/TwitchApp";
 import Aux from "../../hoc/Aux";
 
 class AppShowcase extends Component {
 	state = { page: 1 };
 	
 	advanceShowcasePage = () => {
-		if(this.state.page === 2) {
-			return false;
-		}
 		this.setState(prevState => {
 			return { page: prevState.page + 1 };
 		});
 	};
 
 	backShowcasePage = () => {
-		if(this.state.page === 1) {
-			return false;
-		}
 		this.setState(prevState => {
 			return { page: prevState.page - 1 };
 		});
@@ -43,26 +38,45 @@ class AppShowcase extends Component {
 				</Aux>
 			);
 		}
+		if(this.state.page === 3) {
+			showcase = <TwitchApp/>
+		}
+		let nextBtnClass = "btn app-btn";
+		let backBtnClass = "btn app-btn";
+		if(this.state.page === 3) {
+			nextBtnClass = "btn app-btn disabled";
+		}
+		if(this.state.page === 1) {
+			backBtnClass = "btn app-btn disabled";
+		}
 		return (
 			<div className="section">
 				<div className="card-container">
 						<div className="row center no-margin-top no-margin-bot">
 							<div className="col s6 l4 hide-on-med-and-down">
-								<a className="btn app-btn" onClick={this.backShowcasePage}><i className="material-icons left">navigate_before</i>Back</a>
+								<a className={backBtnClass} onClick={this.backShowcasePage}>
+									<i className="material-icons left">navigate_before</i>Back
+								</a>
 	            </div>
 	            <div className="col s12 l4">
 	              <h3 className="no-margin-top no-margin-bot"><span>App Showcase</span></h3>
 	            </div>
 	            <div className="col s6 l4 hide-on-med-and-down">
-		            <a className="btn app-btn" onClick={this.advanceShowcasePage}>Next<i className="material-icons right">navigate_next</i></a>
+		            <a className={nextBtnClass} onClick={this.advanceShowcasePage}>
+		            	Next<i className="material-icons right">navigate_next</i>
+		            </a>
 	            </div>
 	          </div>
 	          <div className="row hide-on-large-only show-on-medium-and-down center">
 		          <div className="col s6">
-	          		<a className="btn app-btn" onClick={this.backShowcasePage}><i className="material-icons left">navigate_before</i>Previous</a>
+	          		<a className={backBtnClass} onClick={this.backShowcasePage}>
+	          			<i className="material-icons left">navigate_before</i>Previous
+	          		</a>
 	          	</div>
 	          	<div className="col s6">
-	          		<a className="btn app-btn" onClick={this.advanceShowcasePage}>Next<i className="material-icons right">navigate_next</i></a>
+	          		<a className={nextBtnClass} onClick={this.advanceShowcasePage}>
+	          			Next<i className="material-icons right">navigate_next</i>
+	          		</a>
 	          	</div>
 	          </div>
 	          <hr/>
