1219d26de1934f3f9a67c66f4e98cf6d1a2b5091
Terence Mangram <tmangram@gmail.com>
2018-03-12 18:36:42 +0900

    Add recipe app reducer, stores added and local recipes, prevents localStorage from importing to redux more than once

---
+++ src/store/reducers/recipeList.js
@@ -0,0 +1,46 @@
+import * as actionTypes from "../actions/actionTypes";
+
+const initialState = {
+	recipeList: [],
+	retrievedLocal: false
+};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.ADD_RECIPE:
+			return {
+				...state,
+				recipeList: state.recipeList.concat(action.recipe)
+			};
+		case actionTypes.EDIT_RECIPE:
+			const editIndex = state.recipeList.findIndex(recipe => recipe.id === action.id);
+			const editedRecipe = { 
+				...state.recipeList[editIndex],
+				[action.keyName]: action.value
+			};
+			const newRecipeList = [
+				...state.recipeList.slice(0, editIndex),
+				editedRecipe,
+				...state.recipeList.slice(editIndex + 1)
+			];
+			return {
+				...state,
+				recipeList: newRecipeList
+			};
+		case actionTypes.SET_RETRIEVED_STATE:
+			return {
+				...state,
+				retrievedLocal: true
+			};
+		case actionTypes.REMOVE_RECIPE:
+			const updatedRecipeList = state.recipeList.filter(recipe => recipe.id !== action.id);
+			return {
+				...state,
+				recipeList: updatedRecipeList
+			};
+		default:
+			return { ...state };
+	}
+};
+
+export default reducer;
