acb12326b29224844b34712a3410f4d36a2e4aeb
Terence Mangram <tmangram@gmail.com>
2018-03-12 18:31:32 +0900

    Refactor to cleaner code

---
+++ src/containers/WeatherApp/WeatherApp.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Weather from "../../components/Weather/Weather";
+
+class WeatherApp extends Component {
+	state = {
+		loading: true,
+		metric: true,
+		weatherData: null
+	};
+	componentDidMount() {
+		this.getWeatherData();
+	}
+
+	// using navigator.geolocation, get position, request data from FreeCodeCamp Weather API and set it to state and remove loading state
+	getWeatherData = () => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(position => {
+				const lon = position.coords.longitude;
+				const lat = position.coords.latitude;
+				axios
+					.get(
+						"https://fcc-weather-api.glitch.me/api/current?lon=" +
+							lon +
+							"&lat=" +
+							lat
+					)
+					.then(response => {
+						this.setState({
+							loading: false,
+							weatherData: response.data
+						});
+					})
+					.catch(err => {
+						console.log("Error in weather retrieval", err);
+					});
+			});
+		} else {
+			alert("Sorry, geolocation is required for this app to function.");
+		}
+	};
+
+	// record old state for metric, set the state to the opposite of that
+	toggleTempScale = () => {
+		this.setState(prevState => {
+			return { metric: !prevState };
+		});
+	};
+
+	render() {
+		return (
+			<Weather
+				loading={this.state.loading}
+				weatherData={this.state.weatherData}
+				metric={this.state.metric}
+				toggleScale={this.toggleTempScale}
+			/>
+		);
+	}
+}
+
+export default WeatherApp;
