0f09518843e780022684286ffbf9ead707bb2d37
Terence Mangram <tmangram@gmail.com>
2018-03-14 23:07:33 +0900

    UI of CalculatorApp, displays displayInput and messages, onClicks for buttons

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Calculator.css";
+
+const calculator = props => {
+	return (
+		<div id="main-container">
+			<br/>
+			<div id="display-container">
+				<input id="display" type="text" disabled value={props.display.join("")} />
+			</div>
+			<span id="credit">Terence Mangram Instruments</span>
+			<div id="message-container">
+				<input id="message" type="text" disabled value={props.message} />
+				<span id="name">TM-108</span>
+			</div>
+			<br/>
+			<div id="button-container" onClick={props.click}>
+				<button id="clear-all" className="calcButton red">AC</button>
+				<button id="clear-one" className="calcButton red">CE</button>
+				<button id="divide" className="calcButton red" value="/">&divide;</button>
+				<button id="times" className="calcButton red" value="*">&times;</button>
+				<br/>
+				<button id="seven" className="calcButton white" value="7">7</button>
+				<button id="eight" className="calcButton white" value="8">8</button>
+				<button id="nine" className="calcButton white" value="9">9</button>
+				<button id="minus" className="calcButton red" value="-">&minus;</button>
+				<br/>
+				<button id="four" className="calcButton white" value="4">4</button>
+				<button id="five" className="calcButton white" value="5">5</button>
+				<button id="six" className="calcButton white" value="6">6</button>
+				<button id="plus" className="calcButton red" value="+">+</button>
+				<br/>
+				<button id="one" className="calcButton white" value="1">1</button>
+				<button id="two" className="calcButton white" value="2">2</button>
+				<button id="three" className="calcButton white" value="3">3</button>
+				<button id="equals" className="calcButton red" value="=">=</button>
+				<br/>
+				<button id="zero" className="calcButton white" value="0">0</button>
+				<button id="decimal" className="calcButton white" value=".">.</button>
+				<button id="power" className="calcButton red">ON</button>
+			</div>
+			<br/>
+			<br/>
+		</div>
+	);
+};
+
+export default calculator;
