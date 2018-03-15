2bfe0910ae3e6eb3732c1f66fa84b6011bdae2eb
Terence Mangram <tmangram@gmail.com>
2018-03-15 13:07:41 +0900

    Change to new square loading, add CSS file in dir

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,18 @@
 import React from 'react';
+import "./Loading.css";
 
 const loading = (props) => {
 	return (
-		<div className="card-panel center"><br/>
-			<div className="preloader-wrapper active">
-				<div className="spinner-layer spinner-red-only">
-					<div className="circle-clipper left">
-						<div className="circle"></div>
-					</div><div className="gap-patch">
-						<div className="circle"></div>
-					</div><div className="circle-clipper right">
-						<div className="circle"></div>
-					</div>
-				</div>
-			</div>
+		<div className="sk-cube-grid">
+		  <div className="sk-cube sk-cube1"></div>
+		  <div className="sk-cube sk-cube2"></div>
+		  <div className="sk-cube sk-cube3"></div>
+		  <div className="sk-cube sk-cube4"></div>
+		  <div className="sk-cube sk-cube5"></div>
+		  <div className="sk-cube sk-cube6"></div>
+		  <div className="sk-cube sk-cube7"></div>
+		  <div className="sk-cube sk-cube8"></div>
+		  <div className="sk-cube sk-cube9"></div>
 		</div>
 	);
 };
