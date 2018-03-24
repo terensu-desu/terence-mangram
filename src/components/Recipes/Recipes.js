b9f148be8d8623d26a5011daf2a2e6b21afd40e9
Terence Mangram <tmangram@gmail.com>
2018-03-25 00:26:18 +0900

    Add initial set up for edit recipes feature

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -7,6 +7,17 @@ class Recipes extends Component {
 	componentDidMount() {
   	M.Collapsible.init(this.collapsible);
 	}
+	handleEditClick = id => {
+		document.getElementById(id).classList.toggle("hidden");
+		document.getElementById(id+1).classList.toggle("hidden");
+	};
+	handleSaveClick(event, id) {
+		// do Redux edit call
+		event.preventDefault();
+		console.log(event.target.text.value);
+		document.getElementById(id).classList.toggle("hidden");
+		document.getElementById(id-1).classList.toggle("hidden");
+	};
 	render() {
 		const resetStorage = () => {
 			localStorage.clear();
@@ -15,14 +26,14 @@ class Recipes extends Component {
 		const recipesMap = this.props.recipes.map(recipe => (
 			<li key={recipe.id}>
 				<div className="collapsible-header"><i className="material-icons">restaurant</i>{recipe.name}</div>
-	      <div className="collapsible-body recipe-body">
+	      <div id={recipe.id} className="collapsible-body recipe-body">
 		      <ul>
 	      		{recipe.ingredients.map((ingredient, j) => <li key={ingredient+j}>{ingredient}</li>)}
 	      	</ul>
 	      	<hr />
 	      	<p className="no-margin-top article">{recipe.text}</p>
 	      	<button 
-      		data-target={"#modal"+recipe.id}
+      		onClick={()=> this.handleEditClick(recipe.id)}
       		className="btn app-btn left modal-trigger">
       			Edit<i className="material-icons right">edit</i>
 	      	</button>
@@ -32,6 +43,43 @@ class Recipes extends Component {
       			Delete<i className="material-icons right">delete</i>
 	      	</button>
 	      	<br/>
+	      </div>
+	      <div id={recipe.id+1} className="collapsible-body recipe-body hidden">
+	      	<h5 className="center">Edit Mode</h5>
+	      	<hr />
+	      	<form onSubmit={(event)=> this.handleSaveClick(event, recipe.id+1)}>
+			      <div className="row">
+			      	<div className="input-field col s12">
+				      	{/* set up on change event that sends redux info it needs */}
+			      		<textarea
+			      		name="ingredients" 
+			      		id="ingredients" 
+			      		value={recipe.ingredients.join("\n")}
+			      		className="materialize-textarea">
+			      		</textarea>
+			      		<label htmlFor="ingredients">Please keep items separated with a new line (shift + enter).</label>
+			      	</div>
+		      	</div>
+		      	<div className="row">
+			      	<div className="input-field col s12">
+				      	{/* set up on change event that sends redux info it needs */}
+				      	<textarea 
+				      	name="text" 
+				      	id="recipe-text"
+				      	value={recipe.text}
+				      	className="materialize-textarea">.
+				      	</textarea>
+				      	<label htmlFor="recipe-text">Instructions</label>
+			      	</div>
+		      	</div>
+		      	<div className="row">
+			      	<button
+			      	type="submit"
+		      		className="btn app-btn left">
+		      			Save<i className="material-icons right">save</i>
+			      	</button>
+			      </div>
+			    </form>
 	      </div>
 			</li>
 		));
