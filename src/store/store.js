1dbbc1e2fad026a8af717f99a2fe683c48dc21ba
Terence Mangram <tmangram@gmail.com>
2018-03-29 01:09:20 +0900

    Remove redux tools logic

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,11 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import recipeListReducer from "./reducers/recipeList.js";
 
 const rootReducer = combineReducers({
 	recipes: recipeListReducer
 });
-const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-const store = createStore(rootReducer, enhancer);
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
