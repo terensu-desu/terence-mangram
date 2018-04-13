6201f8e828c4012390cf37bc5e83a1e27bec6d3d
Terence Mangram <tmangram@gmail.com>
2018-04-12 20:13:25 -0500

    Change the wording in About section and Buddies

--- src/store/displayData/displayData.js
+++ src/store/displayData/displayData.js
@@ -4,11 +4,11 @@ import BurgerBuilder from "../../assets/images/projects/BurgerBuilder.png";
 const DisplayDataModule = () => {
 	const store = {
 		english: {
-      about: `Born in Spain and raised in Florida, I've just moved to Chicago (April 5th!) after living and working in Japan as a public school English teacher for four years. My journey as a web developer actually ignited while I was still a teacher.
+      about: `Born in Spain and raised in Florida, I've just moved to Chicago (April 5th!) after living and working in Japan as a public school English teacher for four years. My journey as a web developer actually sparked while I was busy teaching phonics and grammar full time.
 
               The first website I created was Chubu ALT, a solution to the problem English teachers have with creating or finding new material to teach with. Although it started with copy and pasted HTML and CSS, I was really proud of what I accomplished. Eventually it would evolve into Nihon ALT - a React based app with a lot more functionality.
 
-              So after all that, I became a student again. From that point on, I've been building fun and challenging projects and try to solve problems I imagine or have had. I've learned a lot, from HTML5, CSS5, and JavaScript to React and Redux to NodeJS and MongoDB, and more.
+              So from that point on, I was a student again. I've been building fun and challenging projects and try to solve problems I imagine or have had. I've learned a lot, from HTML5, CSS5, and JavaScript to React and Redux to NodeJS and MongoDB, and more.
 
               I am actively looking for employment in Chicago, so feel free to contact me! I'd be happy to talk shop, expand on my story, chat about Japan, the language, Formula 1, bouldering, tennis, video games, books, or whatever you like! I'm also active in the local freeCodeCamp group.`,
       projects: [
@@ -22,7 +22,7 @@ const DisplayDataModule = () => {
         {
           title: "Buddies",
           tools: "Made with NodeJS, Express, Mongoose, MongoDB, and MateralizeCSS.",
-          text: "With two friends of mine at Nagoya University, we embarked on a project to demonstrate the need for a website business aimed at helping foreigners in Japan. Inspired by AirBnB and our experiences of living in foreign countries, we dream of launching a website that can enable others to do more in Japan.",
+          text: "With two friends of mine at Nagoya University, we embarked on a project to demonstrate the need for a website business aimed at helping foreigners in Japan. Inspired by AirBnB and our experiences and difficulties living in foreign countries, Buddies aims to solve one of the biggest problems of being a foreigner in Japan.",
           vlink: null,
           ilink: Buddies,
           url: "https://agile-scrubland-23791.herokuapp.com/"
@@ -38,11 +38,11 @@ const DisplayDataModule = () => {
       ]
     },
     japanese: {
-      about: `Born in Spain and raised in Florida, I've just moved to Chicago (April 5th!) after living and working in Japan as a public school English teacher for four years. My journey as a web developer actually ignited while I was still a teacher.
+      about: `Born in Spain and raised in Florida, I've just moved to Chicago (April 5th!) after living and working in Japan as a public school English teacher for four years. My journey as a web developer actually sparked while I was busy teaching phonics and grammar full time.
 
               The first website I created was Chubu ALT, a solution to the problem English teachers have with creating or finding new material to teach with. Although it started with copy and pasted HTML and CSS, I was really proud of what I accomplished. Eventually it would evolve into Nihon ALT - a React based app with a lot more functionality.
 
-              So after all that, I became a student again. From that point on, I've been building fun and challenging projects and try to solve problems I imagine or have had. I've learned a lot, from HTML5, CSS5, and JavaScript to React and Redux to NodeJS and MongoDB, and more.
+              So from that point on, I was a student again. I've been building fun and challenging projects and try to solve problems I imagine or have had. I've learned a lot, from HTML5, CSS5, and JavaScript to React and Redux to NodeJS and MongoDB, and more.
 
               I am actively looking for employment in Chicago, so feel free to contact me! I'd be happy to talk shop, expand on my story, chat about Japan, the language, Formula 1, bouldering, tennis, video games, books, or whatever you like! I'm also active in the local freeCodeCamp group.`,
       projects: [
