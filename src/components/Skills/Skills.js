6e2b6ccb11cebcb988c3c1f5d192af11bce197b2
Terence Mangram <tmangram@gmail.com>
2018-04-13 09:32:46 -0500

    Remove external imported icons, using local svg files instead

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import M from "materialize-css";
 
+import JavaScript from "../../assets/images/skills/javascript.svg";
+import ReactSvg from "../../assets/images/skills/react.svg";
+import ReduxSvg from "../../assets/images/skills/redux.svg";
+import jQuery from "../../assets/images/skills/jquery.svg";
+import Materialize from "../../assets/images/skills/materializecss.svg";
+import Bootstrap from "../../assets/images/skills/bootstrap.svg";
+import NodeJs from "../../assets/images/skills/nodejs.svg";
+import Express from "../../assets/images/skills/express.svg";
+import MongoDB from "../../assets/images/skills/mongodb.svg";
+import npm from "../../assets/images/skills/npm.svg";
+import Git from "../../assets/images/skills/git-icon.svg";
+import Github from "../../assets/images/skills/github-icon.svg";
+import Heroku from "../../assets/images/skills/heroku.svg";
+
 class Skills extends Component {
 	componentDidMount() {
 		M.ScrollSpy.init(this.skills, {scrollOffset: 50});
@@ -24,26 +38,29 @@ class Skills extends Component {
 		      	<div className="row">
 		      		<h4>Front-End</h4>
 		      		<ul className="icon-list">
-		      			<li><i className="devicon-javascript-plain colored"></i></li>
-			      		<li><i className="devicon-react-original-wordmark colored"></i></li>
-			      		<li><i className="devicon-jquery-plain-wordmark colored"></i></li>
-			      		<li><i className="devicon-bootstrap-plain-wordmark colored"></i></li>
+		      			<li><img src={JavaScript} alt="javascript"/></li>
+			      		<li><img src={ReactSvg} alt="react"/></li>
+			      		<li><img src={ReduxSvg} alt="redux"/></li>
+			      		<li><img src={jQuery} alt="jquery"/></li>
+			      		<li><img src={Materialize} alt="materialize"/></li>
+			      		<li><img src={Bootstrap} alt="bootstrap"/></li>
 		      		</ul>
 		      	</div>
 		      	<div className="row">
 		      		<h4>Back-End</h4>
 		      		<ul className="icon-list">
-		      			<li><i className="devicon-nodejs-plain-wordmark colored"></i></li>
-			      		<li><i className="devicon-express-original-wordmark colored"></i></li>
-			      		<li><i className="devicon-mongodb-plain-wordmark colored"></i></li>
+		      			<li><img src={NodeJs} alt="nodejs"/></li>
+			      		<li><img src={Express} alt="express"/></li>
+			      		<li><img src={MongoDB} alt="mongodb"/></li>
 		      		</ul>
 		      	</div>
 		      	<div className="row">
 		      		<h4>Tools</h4>
 		      		<ul className="icon-list">
-			      		<li><i className="devicon-git-plain-wordmark colored"></i></li>
-			      		<li><i className="devicon-github-plain-wordmark colored"></i></li>
-			      		<li><i className="devicon-heroku-original-wordmark colored"></i></li>
+		      			<li><img src={npm} alt="npm"/></li>
+			      		<li><img src={Git} alt="git"/></li>
+			      		<li><img src={Github} alt="github"/></li>
+			      		<li><img src={Heroku} alt="heroku"/></li>
 		      		</ul>
 		      	</div>
 		      </div>
